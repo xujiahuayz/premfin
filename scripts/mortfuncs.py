382003a24385db10a9d5e76d444c40af0407556d
xujiahuayz <31636867+xujiahuayz@users.noreply.github.com>
2021-01-18 12:57:49 +0000

    update setup file

--- scripts/mortfuncs.py
+++ scripts/mortfuncs.py
@@ -1,11 +1,18 @@
 import requests
 from pandas import read_excel
 
-vbt_url = 'https://www.soa.org/globalassets/assets/files/research/exp-study/2015-vbt-unismoke-alb-anb.xlsx'
-r = requests.get(vbt_url)
+VBT_URL = 'https://www.soa.org/globalassets/assets/files/research/exp-study/2015-vbt-unismoke-alb-anb.xlsx'
 
-# need to `pip install openpyxl`
-malevbt = read_excel(
-    r.content, sheet_name='2015 Male Unismoke ANB', header=2, index_col=0)
-femavbt = read_excel(
-    r.content, sheet_name='2015 Female Unismoke ANB', header=2, index_col=0)
+
+def pv_premium(pr, r_free: float = 0.1) -> float:
+
+
+if if __name__ == "__main__":
+    r = requests.get(VBT_URL)
+    # need to `pip install openpyxl`
+    malevbt = read_excel(
+        r.content, sheet_name='2015 Male Unismoke ANB', header=2, index_col=0
+    )
+    femavbt = read_excel(
+        r.content, sheet_name='2015 Female Unismoke ANB', header=2, index_col=0
+    )
