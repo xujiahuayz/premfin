f6264ae1a8cf709bbd4c2a2f851132142fd2377e
SFlashYang <614290752@qq.com>
2022-05-29 14:22:06 +0100

    change List to list

--- premiumFinance/util.py
+++ premiumFinance/util.py
@@ -2,11 +2,10 @@ from typing import Union
 import numpy as np
 import pandas as pd
 from premiumFinance.fetchdata import lapse_tbl
-from typing import List
 
 LIST_LEN = 150
 # make a list with length 150
-def make_list(x, to_length: int = LIST_LEN) -> List[float]:
+def make_list(x, to_length: int = LIST_LEN) -> list[float]:
     if type(x) == np.ndarray:
         x = x.tolist()
     elif type(x) == pd.Series:
@@ -22,9 +21,9 @@ def make_list(x, to_length: int = LIST_LEN) -> List[float]:
 
 
 def cash_flow_pv(
-    cashflow: Union[float, List[float], np.ndarray, pd.Series],
-    probabilities: Union[float, List[float], np.ndarray, pd.Series],
-    discounters: Union[float, List[float], np.ndarray, pd.Series],
+    cashflow: Union[float, list[float], np.ndarray, pd.Series],
+    probabilities: Union[float, list[float], np.ndarray, pd.Series],
+    discounters: Union[float, list[float], np.ndarray, pd.Series],
 ) -> float:
     cashflow = make_list(cashflow)
     probabilities = make_list(probabilities)
@@ -35,7 +34,7 @@ def cash_flow_pv(
 
 
 # lapse rate dependent on gender; lapse == 0 with no lapse assumption
-def lapse_rate(isMale: bool, assume_lapse: bool = True) -> List[float]:
+def lapse_rate(isMale: bool, assume_lapse: bool = True) -> list[float]:
     lapse_rate = [0.0]
     if assume_lapse:
         col_ind = 0 if isMale else 1
