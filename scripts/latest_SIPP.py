7965c312fbbe144efed0b5273817045950a563b5
TammyQAQ <tammywang20020305@gmail.com>
2022-06-26 13:09:41 +0100

    sipp third commit

--- scripts/latest_SIPP.py
+++ scripts/latest_SIPP.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import numpy as np
 
 # Read in the primary data file schema to get data-type information for each variable.
-rd_schema = pd.read_json("pu2020_schema.json")
+rd_schema = pd.read_json("pu2020_schema.json", orient="split")
 
 # Read in the replicate weight data file schema to get data-type information for each variable.
-rw_schema = pd.read_json("rw2020_schema.json")
+rw_schema = pd.read_json("rw2020_schema.json", orient="split")
 
 # Define Pandas data types based on the schema data-type information for both schema dataframes
 rd_schema["dtype"] = [
@@ -78,3 +79,49 @@ df_data = pd.read_csv(
 
 # preview the data
 print(df_data.head())
+
+# check some unweighted means against the validation xls file to help ensure that the data were read in correctly.
+print("TPTOTINC mean:" + str(df_data.TPTOTINC.mean()))
+
+# Read in the replicate-weight data.
+df_rw = pd.read_csv(
+    "rw2020.csv",
+    dtype=dict([(i, v) for i, v in zip(rw_schema["name"], rw_schema["dtype"])]),
+    sep="|",
+    header=0,
+    names=rw_schema["name"],
+)
+
+# preview the data
+print(df_rw.head())
+
+# check some unweighted means against the validation xls file.
+print("REPWT100 mean:" + str(df_rw.REPWGT100.mean()))
+
+# Merge data and replicate weights on SSUID, PNUM, MONTHCODE
+df = df_data.merge(
+    df_rw,
+    left_on=["SSUID", "PNUM", "MONTHCODE"],
+    right_on=["SSUID", "PNUM", "MONTHCODE"],
+)
+
+# preview the merged data
+print(df.head())
+
+# Example of using the replicate weights to estimate the standard error of a weighted mean
+# Requires the NumPy package
+df_est = df.loc[df.TPTOTINC.isna() != True]
+point_estimate = np.nansum(df_est.TPTOTINC * df_est["WPFINWGT"]) / np.nansum(
+    df_est["WPFINWGT"]
+)
+rep_means = [
+    np.nansum(df_est.TPTOTINC * df_est["REPWGT" + str(i)])
+    / np.nansum(df_est["REPWGT" + str(i)])
+    for i in range(1, 241)
+]
+variance = (1 / (240 * 0.5**2)) * sum((rep_means - point_estimate) ** 2)
+print(
+    "Point estimate:{:.2f} , Standard error:{:.2f}".format(
+        point_estimate, variance**0.5
+    )
+)
