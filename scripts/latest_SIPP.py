f87f6488f980540f46c447f44cfb978bdcb6ce1b
TammyQAQ <tammywang20020305@gmail.com>
2022-06-27 20:50:16 +0100

    test

--- scripts/latest_SIPP.py
+++ scripts/latest_SIPP.py
@@ -1,127 +1,69 @@
+#%%
 import pandas as pd
 import numpy as np
 
-# Read in the primary data file schema to get data-type information for each variable.
-rd_schema = pd.read_json("pu2020_schema.json", orient="split")
-
-# Read in the replicate weight data file schema to get data-type information for each variable.
-rw_schema = pd.read_json("rw2020_schema.json", orient="split")
-
-# Define Pandas data types based on the schema data-type information for both schema dataframes
-rd_schema["dtype"] = [
-    "Int64"
-    if x == "integer"
-    else "object"
-    if x == "string"
-    else "Float64"
-    if x == "float"
-    else "ERROR"
-    for x in rd_schema["dtype"]
-]
-
-rw_schema["dtype"] = [
-    "Int64"
-    if x == "integer"
-    else "object"
-    if x == "string"
-    else "Float64"
-    if x == "float"
-    else "ERROR"
-    for x in rw_schema["dtype"]
-]
-
-# Read in the primary data
-df_data = pd.read_csv(
-    "pu2020.csv",
-    names=rd_schema["name"],  # dtype expects a dictionary of key:values
-    dtype=dict(
-        [(i, v) for i, v in zip(rd_schema["name"], rd_schema["dtype"])]
-    ),  # files are pipe-delimited
-    sep="|",
-    header=(),
-    usecols=[
-        "ssuid",
-        "swave",
-        "erelrpe",
-        "wpfinwgt",
-        "tirakeoval",
-        "tthr401val",
-        "tlife_fval",
-        "tlife_cval",
-        "tannval",
-        "tinc_bank",
-        "tval_bank",
-        "tinc_stmf",
-        "tval_stmf",
-        "tinc_bond",
-        "tval_bond",
-        "tinc_rent",
-        "tinc_rent",
-        "teq_rent",
-        "tval_re",
-        "teq_re",
-        "tinc_oth",
-        "tval_oth",
-        "tinc_ast",
-        "tval_ret",
-        "tval_bus",
-        "teq_bus",
-        "tval_home",
-        "teq_home",
-        "tval_veh",
-        "teq_veh",
-        "tval_esav",
-        "tval_rmu",
-        "tval_ast",
-        "tnetworth",
-    ],
+# %%
+data_reader = pd.read_stata(
+    "/Users/tammy/Java_Help/testVS/premfin/data/pu2020.dta",
+    chunksize=1000,
+    columns=["TNETWORTH", "WPFINWGT", "TLIFE_FVAL"],
 )
 
-# preview the data
-print(df_data.head())
+# %%
+frame_1 = pd.DataFrame()
+for i in range(10):
+    chunk_1 = data_reader.get_chunk(1000)
+    chunk_1_filtered = chunk_1.dropna()
+    frame_1 = frame_1.append(chunk_1_filtered)
 
-# check some unweighted means against the validation xls file to help ensure that the data were read in correctly.
-print("TPTOTINC mean:" + str(df_data.TPTOTINC.mean()))
-
-# Read in the replicate-weight data.
-df_rw = pd.read_csv(
-    "rw2020.csv",
-    dtype=dict([(i, v) for i, v in zip(rw_schema["name"], rw_schema["dtype"])]),
-    sep="|",
-    header=0,
-    names=rw_schema["name"],
+#%%
+omd = pd.read_excel(
+    "/Users/tammy/Java_Help/testVS/premfin/data/OrganizedMortalityDistribution.xlsx",
+    header=None,
 )
 
-# preview the data
-print(df_rw.head())
+omd = omd.drop(omd.index[0])  # delete blank row.
+new_header = omd.iloc[0]  # replace header with first row.
+omd = omd[1:]
+omd.columns = new_header
 
-# check some unweighted means against the validation xls file.
-print("REPWT100 mean:" + str(df_rw.REPWGT100.mean()))
-
-# Merge data and replicate weights on SSUID, PNUM, MONTHCODE
-df = df_data.merge(
-    df_rw,
-    left_on=["SSUID", "PNUM", "MONTHCODE"],
-    right_on=["SSUID", "PNUM", "MONTHCODE"],
+omd = omd.drop(
+    columns=[
+        "Amount Exposed ",
+        "Policies Exposed ",
+        "Death Claim Amount ",
+        "Sum of Number of Deaths",
+        "Count of Face Amount Band",
+    ]
 )
 
-# preview the merged data
-print(df.head())
+#%%
+utp = pd.read_excel("/Users/tammy/Java_Help/testVS/premfin/data/untappedprofit.xlsx")
 
-# Example of using the replicate weights to estimate the standard error of a weighted mean
-# Requires the NumPy package
-df_est = df.loc[df.TPTOTINC.isna() != True]
-point_estimate = np.nansum(df_est.TPTOTINC * df_est["WPFINWGT"]) / np.nansum(
-    df_est["WPFINWGT"]
+utp = utp.drop(
+    columns=[
+        "Amount Exposed",
+        "Policies Exposed",
+        "Excess_Policy_PV_0",
+        "Excess_Policy_PV_0.01",
+        "Excess_Policy_PV_0.02",
+        "Excess_Policy_PV_0.05",
+        "Excess_Policy_PV_0.1",
+        "Excess_Policy_PV_0.2",
+        "Excess_Policy_PV_0.5",
+        "Surrender value",
+        "Max Loan rate",
+        "Lender profit",
+        "Dollar profit",
+    ]
 )
-rep_means = [
-    np.nansum(df_est.TPTOTINC * df_est["REPWGT" + str(i)])
-    / np.nansum(df_est["REPWGT" + str(i)])
-    for i in range(1, 241)
-]
-variance = (1 / (240 * 0.5**2)) * sum((rep_means - point_estimate) ** 2)
-print(
-    "Point estimate:{:.2f} , Standard error:{:.2f}".format(
-        point_estimate, variance**0.5
-    )
+
+# change column names to match.
+utp = utp.rename(
+    columns={
+        "isMale": "Gender",
+        "isSmoker": "Smoker Status",
+        "issueage": "Issue Age Group",
+        "currentage": "Attained Age Group",
+    }
 )
