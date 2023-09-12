6d998c799f738012dc0be30f5cc21b5534a0ea8a
JX <jx@JXs-MacBook-Pro.local>
2023-09-12 11:14:19 +0100

    update scripts

--- scripts/process_hrs_data.py
+++ scripts/process_hrs_data.py
@@ -5,3 +5,7 @@ from premiumFinance.constants import DATA_FOLDER
 file_path = DATA_FOLDER / "HRS/h20core/h20sta/H20T_R.dta"
 
 data = pd.read_stata(file_path)
+
+# count of different values in a column "RT040"
+# https://hrs.isr.umich.edu/sites/default/files/meta/2020/core/codebook/h20t_ri.htm
+data["RT040"].value_counts()
